dcd35e17acdd475e9abffd6bed223ec795631b49
Josselin <julien.josselin75@hotmail.fr>
2021-09-09 12:45:07 +0200

    game MVP functionnal

---
+++ Player.gd
@@ -0,0 +1,56 @@
+extends Area2D
+
+signal hit
+
+export var speed = 400.0
+var screen_size = Vector2.ZERO
+
+func _ready():
+	screen_size = get_viewport_rect().size
+	hide()
+
+
+func _process(delta):
+	var direction = Vector2.ZERO
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+
+	if Input.is_action_pressed("move_down"):
+		direction.y += 1
+	if Input.is_action_pressed("move_up"):
+		direction.y -= 1
+
+	# > 0 means player hits at least 1 key. 
+	if direction.length() > 0:
+		# Keep constant speed when move diagonally
+		# (Otherwise you move faster) :
+		direction = direction.normalized()
+		$AnimatedSprite.play() # same as get_node("AnimatedSprite")
+	else:
+		$AnimatedSprite.stop()
+
+	position += direction * speed * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+	
+	if direction.x != 0:
+		$AnimatedSprite.animation = "right"
+		$AnimatedSprite.flip_v = false
+		$AnimatedSprite.flip_h = direction.x < 0
+	elif direction.y != 0:
+		$AnimatedSprite.animation = "up"
+		$AnimatedSprite.flip_v = direction.y > 0
+
+
+func start(new_position):
+	position = new_position
+	show()
+	$CollisionShape2D.disabled = false
+
+
+func _on_Player_body_entered(body):
+	hide()
+	$CollisionShape2D.set_deferred("disabled", true)
+	emit_signal("hit")
---
+++ README.md
@@ -0,0 +1,11 @@
+
+Getting started section from the GodotEngine Docs
+
+## Your first game
+
+Making of game : Dodge The Creeps  
+
+* [Doc ressource from docs.godotengine.org](https://docs.godotengine.org/en/3.4/getting_started/step_by_step/your_first_game.html)
+* [Tutorial from gdquest.com](https://www.gdquest.com/tutorial/godot/learning-paths/getting-started-in-2021/chapter/1.getting-started/)
+
+
