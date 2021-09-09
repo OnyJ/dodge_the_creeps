d34a8d75eade34c4bc980bc658fda04ac112d318
Josselin <julien.josselin75@hotmail.fr>
2021-09-09 13:48:31 +0200

    options button functionnal

--- HUD.gd
+++ HUD.gd
@@ -2,6 +2,7 @@ extends CanvasLayer
 # HUD means Head-up display
 
 signal start_game
+signal options
 
 
 func update_score(score):
@@ -20,13 +21,52 @@ func show_game_over():
 	$MessageLabel.text = "Dodge The Creeps"
 	$MessageLabel.show()
 	yield(get_tree().create_timer(1.0), "timeout")
-	$Button.show()
+	$StartButton.show()
+	$OptionsButton.show()
 
 
-func _on_Button_pressed():
-	$Button.hide()
+func _on_StartButton_pressed():
+	$StartButton.hide()
+	$OptionsButton.hide()
 	emit_signal("start_game")
 
 
 func _on_MessageTimer_timeout():
 	$MessageLabel.hide()
+
+
+# Options Button
+
+func show_options():
+	# show Controls
+	# -> move, start/pause, mute
+	# show SelectDifficulty (1/2/3)
+	# show BackButton
+	pass
+
+func show_start_menu():
+	$ScoreLabel.show()
+	$MessageLabel.show()
+	$StartButton.show()
+
+
+func hide_start_menu():
+	# hide messages and buttons
+	$ScoreLabel.hide()
+	$MessageLabel.hide()
+	$StartButton.hide()
+	pass
+
+
+func switch_options_view():
+	if $OptionsButton.text == "Options":
+		hide_start_menu()
+		$OptionsButton.text = "Back"
+	
+	elif $OptionsButton.text == "Back":
+		show_start_menu()
+		$OptionsButton.text = "Options"
+
+
+func _on_OptionsButton_pressed():
+	switch_options_view()
